ec25d92bc97b29f3b38996c0db6f197a38226946
Sahil Mujawar <156114090+mujawarmsahil@users.noreply.github.com>
2025-10-15 23:22:31 +0530

    Create MiddleNode.java

---
+++ MiddleNode.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;         // move 1 step
+            fast = fast.next.next;    // move 2 steps
+        }
+
+        return slow; // slow now points to the middle node
+    }
+}
